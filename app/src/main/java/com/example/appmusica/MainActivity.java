f69961ae73424de14bcfa893dd85ad2489e054c8
Gustavo Souza <gtssouza94@gmail.com>
2020-07-06 17:27:23 -0300

    Inserindo arquivos, layout finalizado e MediaPlayer executando audio

--- app/src/main/java/com/example/appmusica/MainActivity.java
+++ app/src/main/java/com/example/appmusica/MainActivity.java
@@ -2,13 +2,26 @@ package com.example.appmusica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.media.MediaPlayer;
 import android.os.Bundle;
 
 public class MainActivity extends AppCompatActivity {
+
+    private MediaPlayer mediaPlayer;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        executarSom();
+
+    }
+
+    public void executarSom(){
+        if(mediaPlayer != null){
+            mediaPlayer.start();
+        }
     }
 }
