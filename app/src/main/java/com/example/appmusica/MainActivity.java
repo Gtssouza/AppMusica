d6283e748948837039a09ccf5da6d8eab4b14875
Gustavo Souza <gtssouza94@gmail.com>
2020-07-07 17:40:31 -0300

    AudioManager configurado para a seekBar com volume atual e volume máximo

--- app/src/main/java/com/example/appmusica/MainActivity.java
+++ app/src/main/java/com/example/appmusica/MainActivity.java
@@ -2,14 +2,19 @@ package com.example.appmusica;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.media.AudioManager;
 import android.media.MediaPlayer;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.SeekBar;
 
 public class MainActivity extends AppCompatActivity {
 
     private MediaPlayer mediaPlayer;
+    private SeekBar seekBar;
+    private AudioManager audioManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -18,7 +23,23 @@ public class MainActivity extends AppCompatActivity {
 
 
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.teste);
+        inicializarSeekbar();
+    }
 
+    public void inicializarSeekbar(){
+        seekBar = findViewById(R.id.seekBarVolume);
+
+        //Configura audio manager
+        audioManager = (AudioManager) getSystemService(Context.AUDIO_SERVICE);
+
+        //recupera os dados de valor maximo e valor atual do volume
+        int valumeMax = audioManager.getStreamMaxVolume(AudioManager.STREAM_MUSIC);
+        int volumeAtual = audioManager.getStreamVolume(AudioManager.STREAM_MUSIC);
+
+        //configura os valores maximos para a seekbar
+        seekBar.setMax(valumeMax);
+        //configura valor atual do seekBar
+        seekBar.setProgress(volumeAtual);
 
     }
 
